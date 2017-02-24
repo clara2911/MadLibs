09e4e4eb28ea7493eb01f53b06dec34a8d96cf5b
clara2911 <clara.tump@hotmail.com>
2017-02-24 14:13:54 +0100

    layout landscape in

--- ShowStory.java
+++ ShowStory.java
@@ -3,25 +3,32 @@ package com.example.clara.madlibs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import com.example.stepp.madlibs.Story;
 
 import java.util.ArrayList;
 
 public class ShowStory extends AppCompatActivity {
 
-    ArrayList<String> words;
+    Story story;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_story);
         Intent intent = getIntent();
-        words = intent.getStringArrayListExtra("words");
-        TextView textview1 = (TextView) findViewById(R.id.displayedText1);
-        textview1.setText(words.get(0));
-        TextView textview2 = (TextView) findViewById(R.id.displayedText2);
-        textview2.setText(words.get(1));
-        TextView textview3 = (TextView) findViewById(R.id.displayedText3);
-        textview3.setText(words.get(2));
+        story = (Story) intent.getSerializableExtra("story");
+        TextView textview1 = (TextView) findViewById(R.id.storyTextView);
+        textview1.setText(story.toString());
+    }
+
+    public void goToFirstActivity(View view) {
+        story.clear();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- inputWords.java
+++ inputWords.java
@@ -3,46 +3,92 @@ package com.example.clara.madlibs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.stepp.madlibs.Story;
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class inputWords extends AppCompatActivity {
-
-    ArrayList<String> words;
+    Story story;
+    String storytext;
+    InputStream inFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_words);
+
+
+        // get wordslist variable from homepage
         Intent intent = getIntent();
-        words = intent.getStringArrayListExtra("words");
+        if(intent.getStringExtra("storytext") != null) {
+            storytext = intent.getStringExtra("storytext");
+            story = (Story) intent.getSerializableExtra("story");
+        } else {
+            getStorytext();
+            story.read(inFile);
+        }
+        TextView categoryText = (TextView) findViewById(R.id.category);
+        categoryText.setText(story.getNextPlaceholder());
+        TextView numberRemainingText = (TextView) findViewById(R.id.numberRemaining);
+        String count = Integer.toString(story.getPlaceholderRemainingCount());
+        numberRemainingText.setText(count);
+    }
+
+    public void getStorytext() {
+        inFile = null;
+        try {
+            inFile = txtToStream();
+        } catch(FileNotFoundException e) {
+            Log.d("File not found", "File not found");
+        }
+        story = new Story(inFile);
+        storytext = story.toString();
+    }
+
+    public InputStream txtToStream()
+        throws FileNotFoundException {
+//            File initialFile = new File("res/raw/simp.txt");
+//            InputStream is = null;
+//            try {
+//                is = new FileInputStream(initialFile);
+//            } catch(FileNotFoundException e) {
+//                Log.d("FileNotFoundException","File not found");
+//            }
+//        return is;
+        String str = "<Male-Name> has announced that his <adjective>\n" +
+                "clothing store in the heart of downtown <CITY> is having\n" +
+                "a/an <adjective> sale of all merchandise, including\n" +
+                "<unusual-adjective> suits and slightly irregular <plural-noun>\n" +
+                "available. Men's cable-knit <plural-noun> , only $15.99.\n" +
+                "Hand-woven Italian <plural-noun> , 1/2-price. Double-\n" +
+                "breasted cashmere <plural-noun> , $50.00. Genuine imported\n" +
+                "<Color!> <adjective> shoes, <Exciting-adjective> handerchiefs,\n" +
+                "and women's embroidered <plural-noun> , all at rock-bottom prices.\n" +
+                "This is a chance to get some really <Interesting-Adjective> bargains.";
+        return new ByteArrayInputStream(str.getBytes());
     }
 
     public void goToSecondActivityAgain(View view) {
         EditText wordObject = (EditText) findViewById(R.id.editText2);
         String word = wordObject.getText().toString();
-        words.add(word);
+        story.fillInPlaceholder(word);
         Intent intent;
-        if(words.size() < 3) {
+        if(story.getPlaceholderRemainingCount() > 0) {
             intent = new Intent(this, inputWords.class);
         } else {
             intent = new Intent(this, ShowStory.class);
         }
-        intent.putExtra("words", words);
-        startActivity(intent);
-        finish();
-
-
-    }
-
-
-    public void goToThirdActivity(View view) {
-        Intent intent = new Intent(this, ShowStory.class);
-        intent.putExtra("wordsList", words);
+        intent.putExtra("storytext", storytext);
+        intent.putExtra("story", story);
         startActivity(intent);
         finish();
     }
+
 }
